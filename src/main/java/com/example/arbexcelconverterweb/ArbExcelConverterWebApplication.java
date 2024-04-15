a4d76242e8e211f036d387f287e1542f2d0331de
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2024-04-15 14:53:35 +0200

    init

---
+++ src/main/java/com/example/arbexcelconverterweb/ArbExcelConverterWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.arbexcelconverterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbExcelConverterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbExcelConverterWebApplication.class, args);
+	}
+
+}
