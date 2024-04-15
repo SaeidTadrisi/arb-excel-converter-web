f7d58e747e01ceae584c329b14836e48c81fed5c
aliJavaTar <ali77erfagh99@gmail.com>
2024-04-15 20:02:24 +0330

    add controller , add dto , add ReadArbAndConvertToExcel

---
+++ src/main/java/com/example/arbexcelconverterweb/application/ReadArbAndConvertToExcel.java
@@ -0,0 +1,20 @@
+package com.example.arbexcelconverterweb.application;
+
+import com.example.arbexcelconverterweb.domain.ARBFile;
+import com.example.arbexcelconverterweb.presentation.dto.ArbRequest;
+import com.example.arbexcelconverterweb.presentation.dto.ExelResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReadArbAndConvertToExcel {
+
+    public ExelResponse conventToExcel(ArbRequest arbRequest) {
+        ARBFile arbFile = new ARBFile(arbRequest.getFileList());
+        List<String> strings = arbFile.arbToString();
+        //
+        return new ExelResponse();
+        //
+    }
+}
---
+++ src/main/java/com/example/arbexcelconverterweb/presentation/dto/ArbRequest.java
@@ -0,0 +1,11 @@
+package com.example.arbexcelconverterweb.presentation.dto;
+
+import lombok.Getter;
+
+import java.io.File;
+import java.util.List;
+
+@Getter
+public class ArbRequest {
+    private List<File> fileList;
+}
