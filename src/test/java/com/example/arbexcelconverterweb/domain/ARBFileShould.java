958a4a1385b659ee22b5dbd8b042c0cc489855cd
Saeid Tadrisi <Saeid.tadrisi@gmail.com>
2024-04-15 18:48:35 +0330

    change name

--- src/test/java/com/example/arbexcelconverterweb/domain/ARBFileShould.java
+++ src/test/java/com/example/arbexcelconverterweb/domain/ARBFileShould.java
@@ -3,7 +3,6 @@ package com.example.arbexcelconverterweb.domain;
 import com.example.arbexcelconverterweb.domain.exception.InvalidFileExtensionException;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +17,7 @@ class ARBFileShould {
     }
 
     @Test
-    void should_Throws_extcption_when_file_extension_is_not_valid() {
+    void should_Throws_exception_when_file_extension_is_not_valid() {
         //todo writ arb file in list -> list of......
         assertThrows(InvalidFileExtensionException.class, () -> new ARBFile(List.of()));
     }
