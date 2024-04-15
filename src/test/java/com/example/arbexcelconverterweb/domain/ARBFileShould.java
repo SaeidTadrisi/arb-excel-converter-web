8ff7ce68570904f283526a1db5ea458239fb6313
aliJavaTar <ali77erfagh99@gmail.com>
2024-04-15 18:33:37 +0330

    add test and check arb file

---
+++ src/test/java/com/example/arbexcelconverterweb/domain/ARBFileShould.java
@@ -0,0 +1,20 @@
+package com.example.arbexcelconverterweb.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ARBFileShould {
+    @Test
+    void should_convert_arb_file_to_string() {
+        ARBFile arbFile = new ARBFile();
+//        arbFile.arbToString();
+    }
+
+    @Test
+    void should_arb_file_has_last_character__to_string() {
+        ARBFile arbFile = new ARBFile();
+//        arbFile.arbToString();
+    }
+
+}
