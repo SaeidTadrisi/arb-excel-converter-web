b0625030606e63a0cb072fb292590742ca51da83
Saeid Tadrisi <Saeid.tadrisi@gmail.com>
2024-04-15 18:47:13 +0330

    refactor class arb and add new test

--- src/test/java/com/example/arbexcelconverterweb/domain/ARBFileShould.java
+++ src/test/java/com/example/arbexcelconverterweb/domain/ARBFileShould.java
@@ -1,20 +1,27 @@
 package com.example.arbexcelconverterweb.domain;
 
+import com.example.arbexcelconverterweb.domain.exception.InvalidFileExtensionException;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ARBFileShould {
     @Test
     void should_convert_arb_file_to_string() {
-        ARBFile arbFile = new ARBFile();
-//        arbFile.arbToString();
+        //todo writ arb file in list -> list of......
+        ARBFile arbFile = new ARBFile(List.of());
+
+        assertDoesNotThrow(arbFile::arbToString);
     }
 
     @Test
-    void should_arb_file_has_last_character__to_string() {
-        ARBFile arbFile = new ARBFile();
-//        arbFile.arbToString();
+    void should_Throws_extcption_when_file_extension_is_not_valid() {
+        //todo writ arb file in list -> list of......
+        assertThrows(InvalidFileExtensionException.class, () -> new ARBFile(List.of()));
     }
+
 
 }
