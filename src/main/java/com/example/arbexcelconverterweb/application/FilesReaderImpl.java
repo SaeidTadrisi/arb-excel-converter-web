7dc37bd05bb37a2b1928a8a863e6e243a8a5be71
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2024-04-19 14:13:22 +0200

    Orchestrator class changed, FilesReader interface created.

---
+++ src/main/java/com/example/arbexcelconverterweb/application/FilesReaderImpl.java
@@ -0,0 +1,13 @@
+package com.example.arbexcelconverterweb.application;
+
+import java.io.File;
+import java.util.List;
+
+public class FilesReaderImpl implements FilesReader {
+    @Override
+    public String read(List<File> fileList, String referenceFile) {
+
+
+        return "";
+    }
+}
