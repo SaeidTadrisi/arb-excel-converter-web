bc18b26ee5fd20e9c7e72c43115e9e9272d9d1bd
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2024-05-31 17:05:57 +0200

    CrossOrigin annotation added.

--- src/main/java/com/example/arbexcelconverterweb/controller/ConvertTranslationController.java
+++ src/main/java/com/example/arbexcelconverterweb/controller/ConvertTranslationController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
@@ -25,6 +22,7 @@ import java.util.zip.ZipOutputStream;
 import static java.util.Objects.requireNonNull;
 
 @RestController
+@CrossOrigin(origins = "https://saeidtadrisi.github.io/arb-converter/", methods = { RequestMethod.POST }, allowedHeaders = { "Content-Type" })
 @RequestMapping("/translate")
 public class ConvertTranslationController {
 
--- src/main/java/com/example/arbexcelconverterweb/controller/PrepareToTranslateController.java
+++ src/main/java/com/example/arbexcelconverterweb/controller/PrepareToTranslateController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "https://saeidtadrisi.github.io/arb-converter/", methods = { RequestMethod.POST }, allowedHeaders = { "Content-Type" })
 @RequestMapping("/translate")
 public class PrepareToTranslateController {
 
